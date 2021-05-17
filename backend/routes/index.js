ea30a7a3a101efa27450e3587f8fe0abbfd40df0
Ethan <ethanhilljay@outlook.com>
2021-05-17 17:44:36 +0100

    Added main dishes recipes endpoint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -13,4 +13,18 @@ router.get("/recipes", (req, res) => {
   res.send(data)
 })
 
+router.get("/recipes/mains", (req, res) => {
+  dir = path.join(__dirname, "../data/")
+  const data = fs
+    .readdirSync(dir)
+    .filter((name) => path.extname(name) === ".json")
+    .map((name) => require(path.join(dir, name)))
+
+  const mains = data.filter((dish) => {
+    return dish.tags.includes("main dish")
+  })
+
+  res.send(mains)
+})
+
 module.exports = router
