9ee0694710f2c734eab8fb579e22e5b035142ee3
Ethan <ethanhilljay@outlook.com>
2021-05-17 18:05:43 +0100

    Added recipe and search pages and created recipe ID fetch request

--- frontend/build/script.js
+++ frontend/build/script.js
@@ -10,6 +10,13 @@ const toggleMenu = (e) => {
   }
 }
 
+var input = document.getElementById("search")
+input.addEventListener("keydown", function (event) {
+  if (event.keyCode === 13) {
+    search(event.target.value.toLowerCase())
+  }
+})
+
 var pathArray = window.location.pathname.split("/")
 const urlParams = new URLSearchParams(window.location.search)
 const ID = urlParams.get("id")
@@ -71,7 +78,21 @@ switch (pathArray[pathArray.length - 1]) {
         displayRecipes(data)
       })
       .catch((error) => console.error("FETCH ERROR:", error))
-    break     
+    break
+  case "recipe.html":
+    fetch(`http://localhost:3000/api/recipes/${ID}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error("No recipe found")
+        }
+      })
+      .then((data) => {
+        displayRecipe(data)
+      })
+      .catch((error) => console.error("FETCH ERROR:", error))
+    break       
 }
 
 function displayRecipes(data) {
@@ -116,4 +137,105 @@ function displayRecipes(data) {
    </div>
   `
   })
+}
+
+function displayRecipe(data) {
+  data.forEach((recipe) => {
+    const recipeCard = document.querySelector(".recipe")
+
+    let tags = ""
+    let ingredients = ""
+    let directions = ""
+
+    for (let index = 0; index < recipe.tags.length; index++) {
+      tags =
+        tags +
+        `<span class="m-1 bg-gray-200 hover:bg-gray-300 rounded-full px-2 font-bold text-sm leading-loose cursor-pointer">${recipe.tags[index]}</span>
+		  `
+    }
+
+    for (let index = 0; index < recipe.ingredients.length; index++) {
+      ingredients =
+        ingredients +
+        `<li class="my-1 bg-green-200 rounded-md px-2 md:w-3/4 font-light text-sm leading-loose ">${recipe.ingredients[index]}</li>
+			`
+    }
+
+    for (let index = 0; index < recipe.directions.length; index++) {
+      directions =
+        directions +
+        `<li class="my-2 bg-red-200 rounded-md md:w-3/4 px-2 font-light text-sm leading-loose ">${recipe.directions[index]}</li>
+			  `
+    }
+
+    // Attemping to increase values
+
+    function increase(amount) {
+      let ValsArray = []
+      for (let index = 0; index < recipe.ingredients.length; index++) {
+        let tempArr = []
+        const element = recipe.ingredients[index].match(/\d+([\/.]\d+)?/g)
+        const value = element.reduce((a, b) => eval(a) + eval(b))
+        const decimalValue = eval(value)
+        console.log(
+          recipe.ingredients[index].replace(
+            /\d+([\/.]\d+)?/g,
+            decimalValue * amount
+          )
+        )
+      }
+    }
+
+    increase(2)
+
+    recipeCard.innerHTML =
+      recipeCard.innerHTML +
+      `
+	  <div
+	  class="container m-2" id="${recipe.id}"
+	  >
+	  <div class="p-4">
+		 <a href="${
+       recipe.source_url
+     }" class="my-2 text-2xl font-medium text-gray-600 uppercase">
+		 ${recipe.title}
+		 </a>
+		 <div class="flex">
+			<h4 class="mr-2">Prep time: ${recipe.prep_time_min || "NaN"}</h4>
+			<h4 class="mr-2">Servings: ${recipe.servings || "NaN"}</h4>
+			<h4 class="mr-2">Cook time: ${recipe.cook_time_min || "NaN"}</h4>
+		 </div>
+		 <div class="flex flex-wrap my-3 -m-1 tags">${tags}</div>
+		 <p class="text-justify">${recipe.description}</p>
+		 <h2 class="text-justify mt-5 mb-2 text-xl font-medium text-gray-600 uppercase">Ingredients</h2>
+		 <ul class="text-justify">
+			${ingredients}
+		 </ul>
+		 <br />
+		 <h2 class="text-justify my-1 text-xl font-medium text-gray-600 uppercase">Directions</h2>
+		 <ol class="text-justify list-decimal">
+			${directions}
+		 </ol>
+	  </div>
+	  <hr />
+	  <br />
+	  <h2 class="my-2 text-2xl font-medium text-gray-600 uppercase">Author</h2>
+	  <div>
+		 <a href="${
+       recipe.author.url
+     }" class="text-justify mb-2 text-lg text-gray-600">${
+        recipe.author.name
+      }</a>
+	  </div>
+   </div>
+	`
+  })
+}
+
+function navigate(id) {
+  window.location.href = `${window.location.origin}/frontend/build/recipe.html?id=${id}`
+}
+
+function search(content) {
+  window.location.href = `${window.location.origin}/frontend/build/search.html?search=${content}`
 }
